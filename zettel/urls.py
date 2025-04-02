057bb20614ad3f8cdebd5d00bc5067d323cb2650
Jakob Laptop VS Code <jakob.sinn@posteo.de>
2025-04-02 19:46:25 +0200

    Probleme gefixt plus kleine kosmetik

--- zettel/urls.py
+++ zettel/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /polls/
     path("", views.UniqueGremiumListView.as_view(), name="index"),
     path("basisform", views.KandiView),
     path("<path:gremiumwanted>", views.KandiView),
