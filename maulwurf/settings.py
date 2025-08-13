3127d531729ffd0da184f56a923fe5eff7531967
Jakob Laptop VS Code <jakob.sinn@posteo.de>
2025-08-13 12:08:39 +0200

    Fix für problem, dass auch kandis ohne vornamen gedruckt werden

--- maulwurf/settings.py
+++ maulwurf/settings.py
@@ -25,60 +25,60 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-#TODO: #5 Allowed_hosts korrekt einstellen
+# TODO: #5 Allowed_hosts korrekt einstellen
 ALLOWED_HOSTS = ("*",)
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'zettel',
-    'django_bootstrap5'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "zettel",
+    "django_bootstrap5",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'maulwurf.urls'
+ROOT_URLCONF = "maulwurf.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'maulwurf.wsgi.application'
+WSGI_APPLICATION = "maulwurf.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -91,15 +91,15 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {    
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
             "NAME": "wahlen",
             "USER": "stimmzettel",
             "PASSWORD": os.getenv("DBKEY"),
             "HOST": "stura.uni-heidelberg.de",
             "PORT": "3306",
-    }
+        }
     }
 
 # Password validation
@@ -107,25 +107,25 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "de"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
@@ -134,10 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- zettel/views.py
+++ zettel/views.py
@@ -8,83 +8,138 @@ from .stimmzettelgenerator import zetteldrucken, heutestr, bericht
 
 
 class UniqueGremiumListView(ListView):
-    '''
+    """
     Dieser View gibt eine Liste aller Ämter, für die Kandidaturen existieren aus
-    '''
+    """
+
     model = Kandidatur
-    template_name = 'zettel/index.html'
-    context_object_name = 'gremium_list'
+    template_name = "zettel/index.html"
+    context_object_name = "gremium_list"
 
     def get_queryset(self):
         try:
-            return Kandidatur.objects.filter(bestaetigt='ja').filter(gewaehlt='noch nicht').values('gremium').distinct()
+            return (
+                Kandidatur.objects.filter(bestaetigt="ja")
+                .filter(gewaehlt="noch nicht")
+                .values("gremium")
+                .distinct()
+            )
         except:  # noqa: E722
             return {}
 
-def KandiView(request, gremiumwanted=''):
-    if request.method == 'POST':
+
+def KandiView(request, gremiumwanted=""):
+    if request.method == "POST":
         stimmzettel_eigenschaften_form = StimmzettelEigenschaftenForm(request.POST)
         kandidaten_form = NameFormSet(request.POST)
 
-        if not (stimmzettel_eigenschaften_form.is_valid() and kandidaten_form.is_valid()):
-            return HttpResponseServerError('Die Eingaben gefallen dem Server nicht.')
+        if not (
+            stimmzettel_eigenschaften_form.is_valid() and kandidaten_form.is_valid()
+        ):
+            return HttpResponseServerError("Die Eingaben gefallen dem Server nicht.")
 
         stimmzettel_eigenschaften = stimmzettel_eigenschaften_form.cleaned_data
-        kandidaten = [nd.cleaned_data for nd in kandidaten_form if nd.cleaned_data]
+        kandidaten = [
+            nd.cleaned_data
+            for nd in kandidaten_form
+            if nd.cleaned_data.get("first_name")
+        ]
 
-        kandidaten_namen = list(map(
-            lambda kandidat: kandidat['first_name'] + ' ' + kandidat['last_name'], kandidaten))
+        kandidaten_namen = list(
+            map(
+                lambda kandidat: kandidat["first_name"] + " " + kandidat["last_name"],
+                kandidaten,
+            )
+        )
 
-        kandidaten_kommentare = list(map(
-            lambda kandidat: kandidat['candidate_comment'], kandidaten))
+        kandidaten_kommentare = list(
+            map(lambda kandidat: kandidat["candidate_comment"], kandidaten)
+        )
 
         # Wenn keine Kampfkandidatur vorliegt, wird bei allen Kandidaten mit ja / nein / enth. abgestimmt.
-        is_jnewahl = not (len(kandidaten_namen) > stimmzettel_eigenschaften['anzahl_stimmen'])
+        is_jnewahl = not (
+            len(kandidaten_namen) > stimmzettel_eigenschaften["anzahl_stimmen"]
+        )
 
-        if 'willstimmzettel' in request.POST:
+        if "willstimmzettel" in request.POST:
             # Stimmzettel generieren" wurde gedrückt
             try:
-                response = (HttpResponse(zetteldrucken(kandidaten_namen, stimmzettel_eigenschaften['gremien_name'], abstimmungsdatum=stimmzettel_eigenschaften['abstimmung_datum'],
-                                                       extras=kandidaten_kommentare,
-                                                       stimmen=stimmzettel_eigenschaften['anzahl_stimmen'],
-                                                       jne=is_jnewahl, printbel=stimmzettel_eigenschaften['belehrung_drucken']),
-                                         content_type='application/pdf'))
-                response['Content-Disposition'] = "attachment; filename=" + stimmzettel_eigenschaften[
-                    'gremien_name'] + heutestr() + 'Stimmzettel.pdf'
+                response = HttpResponse(
+                    zetteldrucken(
+                        kandidaten_namen,
+                        stimmzettel_eigenschaften["gremien_name"],
+                        abstimmungsdatum=stimmzettel_eigenschaften["abstimmung_datum"],
+                        extras=kandidaten_kommentare,
+                        stimmen=stimmzettel_eigenschaften["anzahl_stimmen"],
+                        jne=is_jnewahl,
+                        printbel=stimmzettel_eigenschaften["belehrung_drucken"],
+                    ),
+                    content_type="application/pdf",
+                )
+                response["Content-Disposition"] = (
+                    "attachment; filename="
+                    + stimmzettel_eigenschaften["gremien_name"]
+                    + heutestr()
+                    + "Stimmzettel.pdf"
+                )
                 return response
-            except Exception as e: 
+            except Exception as e:
                 print(e)
-                return HttpResponseServerError("Leider ist beim Erstellen des Berichts etwas schiefgelaufen, sorry! " + repr(e))
-        if 'willbericht' in request.POST:
+                return HttpResponseServerError(
+                    "Leider ist beim Erstellen des Berichts etwas schiefgelaufen, sorry! "
+                    + repr(e)
+                )
+        if "willbericht" in request.POST:
             # Wakobericht wurde gedrückt
             try:
                 response = HttpResponse(
-                    bericht(kandidaten=kandidaten_namen, posten=stimmzettel_eigenschaften['gremien_name'],
-                            jne=is_jnewahl, abstimmungsdatum=stimmzettel_eigenschaften['abstimmung_datum']),
-                    content_type='application/pdf')
-                response['Content-Disposition'] = "attachment; filename=" + stimmzettel_eigenschaften[
-                    'gremien_name'] + heutestr() + 'Berichtvorlage.pdf'
+                    bericht(
+                        kandidaten=kandidaten_namen,
+                        posten=stimmzettel_eigenschaften["gremien_name"],
+                        jne=is_jnewahl,
+                        abstimmungsdatum=stimmzettel_eigenschaften["abstimmung_datum"],
+                    ),
+                    content_type="application/pdf",
+                )
+                response["Content-Disposition"] = (
+                    "attachment; filename="
+                    + stimmzettel_eigenschaften["gremien_name"]
+                    + heutestr()
+                    + "Berichtvorlage.pdf"
+                )
                 return response
-            except Exception as e: 
+            except Exception as e:
                 print(e)
-                return HttpResponseServerError("Leider ist beim Erstellen des Berichts etwas schiefgelaufen, sorry! " + repr(e))
+                return HttpResponseServerError(
+                    "Leider ist beim Erstellen des Berichts etwas schiefgelaufen, sorry! "
+                    + repr(e)
+                )
     else:
-        namelist = Kandidatur.objects.filter(bestaetigt='ja').filter(gewaehlt='noch nicht').filter(
-            gremium=gremiumwanted)
+        namelist = (
+            Kandidatur.objects.filter(bestaetigt="ja")
+            .filter(gewaehlt="noch nicht")
+            .filter(gremium=gremiumwanted)
+        )
         initialname = []
         for n in namelist:
             initialname.append(
-                {'first_name': n.first_name,
-                 'last_name': n.last_name,
-                 'candidate_comment': n.fakultaetFachschaft,
-                 'lesung': n.lesung or " ACHTUNG: Keine Daten in DB!"
-                 }
+                {
+                    "first_name": n.first_name,
+                    "last_name": n.last_name,
+                    "candidate_comment": n.fakultaetFachschaft,
+                    "lesung": n.lesung or " ACHTUNG: Keine Daten in DB!",
+                }
             )
-        initialdata = {'gremien_name': gremiumwanted, 'stimmenzahl': len(initialname)}
-        stimmzettel_eigenschaften_form = StimmzettelEigenschaftenForm(initial=initialdata)
+        initialdata = {"gremien_name": gremiumwanted, "stimmenzahl": len(initialname)}
+        stimmzettel_eigenschaften_form = StimmzettelEigenschaftenForm(
+            initial=initialdata
+        )
         if gremiumwanted:
             kandidaten_form = NameFormSet(initial=initialname)
         else:
             kandidaten_form = ExtraFormSet()
-    return render(request, "zettel/form.html",
-                  {'DataForm': stimmzettel_eigenschaften_form, 'NameForm': kandidaten_form})
+    return render(
+        request,
+        "zettel/form.html",
+        {"DataForm": stimmzettel_eigenschaften_form, "NameForm": kandidaten_form},
+    )
